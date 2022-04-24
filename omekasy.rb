5a3eddf436ff1bc689a8a9bb5b09e7cf8a76d1f5
ikanago <ikanago-dev@protonmail.com>
2022-04-24 21:32:02 +0900

    Fix binary URL

--- omekasy.rb
+++ omekasy.rb
@@ -5,10 +5,10 @@ class Omekasy < Formula
   license "MIT"
 
   if OS.mac?
-    url "#{homepage}/releases/download/v#{version}/omekasy-#{version}-x86_64-apple-darwin.tar.gz"
+    url "#{homepage}/releases/download/v#{version}/omekasy-v#{version}-x86_64-apple-darwin.tar.gz"
     sha256 "c516bb17ca32f4ff97d5ab233d21bcfcf8a19d5b88d9667d1c09c5dcfaad1431"
   elsif OS.linux?
-    url "#{homepage}/releases/download/v#{version}/omekasy-#{version}-x86_64-unknown-linux-gnu.tar.gz"
+    url "#{homepage}/releases/download/v#{version}/omekasy-v#{version}-x86_64-unknown-linux-gnu.tar.gz"
     sha256 "7214ab43c583a7e531a1ab932468cc775dc20e5baa81869bfe6608ef6c522cb2"
   end
 
